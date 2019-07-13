00ebf61dc783b290adeaf3ede6f38be101df93ba
qingyun <zheng2222021713@hotmail.com>
2019-07-13 16:11:39 +0800

    位置类型增删改查

--- src/views/Assetsetting/positionType.vue
+++ src/views/Assetsetting/positionType.vue
@@ -9,17 +9,17 @@
               <el-input v-model="positionTypeFormSearce.text" placeholder="全局查询" size="small" />
               <el-button type="primary" size="small" @click="getData()">查询</el-button>
               <el-button type="success" size="small" @click="adddata()">添加</el-button>
-              <el-button type="warning" size="small" @click="updatedata()">修改</el-button>
-              <el-button type="danger" size="small" @click="deletdata()">删除</el-button>
+              <el-button type="warning" size="small" @click="updatePositionType()">修改</el-button>
+              <el-button type="danger" size="small" @click="deletePositionType()">删除</el-button>
             </div>
           </div>
           <div class="content">
-            <el-table :data="positionTypeData" stripe border style="width: 100%" @selection-change="handleSelectionChange">
+            <el-table :data="positionTypeData" stripe border style="width: 100%" @selection-change="handleSelectionChangePositionType">
               <el-table-column type="selection" width="40" />
               <el-table-column prop="id" label="序号" />
               <el-table-column prop="name" label="位置类型名称" />
             </el-table>
-            <pagination v-show="positionTypeTotalCount>0" :total="positionTypeTotalCount" :page.sync="positionTypeFormSearce.pageNumber" :limit.sync="positionTypeFormSearce.pageSize" @pagination="getpositionTypePage" />
+            <pagination v-show="positionTypeTotalCount>0" :total="positionTypeTotalCount" :page.sync="positionTypeFormSearce.pageNumber" :limit.sync="positionTypeFormSearce.pageSize" @pagination="getPositionTypePage" />
 
             <el-dialog :title="positionTypeFormTitle" :visible.sync="positionTypeFormVisible" :close-on-press-escape="false" :close-on-click-modal="false" width="450px" @close="positionTypeFormClose">
               <el-form ref="positionTypeForm" :model="positionTypeForm" :rules="positionTypeFormRules" label-width="120px">
@@ -29,15 +29,15 @@
               </el-form>
               <span slot="footer" class="dialog-footer">
                 <el-button type="primary" @click="positionTypeFormVisible=false">关闭</el-button>
-                <el-button type="primary" @click="submitData()">提交</el-button>
+                <el-button type="primary" @click="submitPositionType()">提交</el-button>
               </span>
             </el-dialog>
 
-            <el-dialog ref="removeData" title="提示" :close-on-press-escape="false" :close-on-click-modal="false" :visible.sync="removeQuestionVisible" width="220px">
+            <el-dialog ref="removeData" title="提示" :close-on-press-escape="false" :close-on-click-modal="false" :visible.sync="positionTypeDeleteModelVisible" width="220px">
               <span>您确定要删除此条数据？</span>
               <span slot="footer" class="dialog-footer">
-                <el-button @click="removeQuestionVisible = false">取 消</el-button>
-                <el-button type="primary" @click="removeQuestion">确 定</el-button>
+                <el-button @click="positionTypeDeleteModelVisible = false">取 消</el-button>
+                <el-button type="primary" @click="submitDeletepositionType">确 定</el-button>
               </span>
             </el-dialog>
           </div>
@@ -74,17 +74,9 @@ export default {
           trigger: 'blur'
         }
       },
-      // /////////////////////////
-      removeData: null, // 当前表单所选删除行
-      tableData: [], // 全部数据
-      removeQuestionVisible: false, // 删除弹框隐藏
-      searchMessage: '', // 全局搜索的值
-      multipleSelection: '', // 当前表单所选行val
-      changeActiveVisible: false, // 添加弹出框隐藏
-      title: '添加位置类型', // 弹框标题
-      formSearch: {// 弹框表单数据
-        type: ''
-      }
+      positionTypeDeleteModelVisible: false,
+      positionTypeDeleteDataId: null,
+      multipleSelectionpPositionType: []
     }
   },
   mounted() {
@@ -99,7 +91,7 @@ export default {
       })
     },
     // 分页
-    getpositionTypePage(val) {
+    getPositionTypePage(val) {
       // 展示条数
       this.positionTypeFormSearce.pageSize = val.limit
       // 页码
@@ -107,62 +99,76 @@ export default {
       // 调用获取数据
       this.getData()
     },
+    // 表单关闭重置
+    positionTypeFormClose() {
+      this.$refs.positionTypeForm.resetFields()
+    },
     // 添加
     adddata() {
       this.positionTypeFormVisible = true// 显示弹框
       this.positionTypeFormTitle = '添加位置类型'
     },
-    // 提交表单
-    submitData() {
-      // 添加弹出框点确认方法
+    updatePositionType(row) {
+      if (row === undefined) {
+        this.positionTypeFormTitle = '编辑'
+        if (this.multipleSelectionPositionType.length !== 1) {
+          this.$message.error('请选择一项数据进行操作')
+        } else {
+          this.positionTypeFormVisible = true
+          this.positionTypeForm.id = this.multipleSelectionPositionType[0].id
+          this.positionTypeForm.name = this.multipleSelectionPositionType[0].name
+        }
+      } else {
+        this.positionTypeFormVisible = true
+        this.positionTypeForm.id = row.id
+        this.positionTypeForm.name = row.name
+      }
+    },
+    // 表单提交
+    submitPositionType() {
       this.$refs.positionTypeForm.validate(valid => {
         if (valid) {
-          this.$axios.post('/', this.positionTypeForm).then(res => {
-            this.getpositionTypeData()
-            this.positionTypeFormVisible = false
-          })
+          if (this.positionTypeForm.id === undefined) {
+            this.$axios.post('/api/Meta/PositionType', this.positionTypeForm).then(res => {
+              this.getData()
+              this.$message.success('添加成功')
+              this.positionTypeFormVisible = false
+            })
+          } else {
+            this.$axios.put('/api/Meta/PositionType/' + this.positionTypeForm.id, this.positionTypeForm).then(res => {
+              this.getData()
+              this.$message.success('修改成功')
+              this.positionTypeFormVisible = false
+            })
+          }
         }
       })
     },
-    // 表单关闭重置
-    positionTypeFormClose() {
-      this.$refs.positionTypeForm.resetFields()
-    },
-    // ////////////////////////////////////////////
-
-    updatedata() {
-      // 修改方法
-      if (this.multipleSelection === '') {
-        this.$message.error('请至少选择一条数据')
+    // 删除
+    deletePositionType(row) {
+      if (row === undefined) {
+        if (this.multipleSelectionPositionType.length !== 1) {
+          this.$message.error('请选择一项数据进行操作')
+        } else {
+          this.positionTypeDeleteModelVisible = true
+          this.positionTypeDeleteDataId = this.multipleSelectionPositionType[0].id
+        }
       } else {
-        this.title = '编辑位置类型'
-        this.changeActiveVisible = true// 显示弹框
+        this.positionTypeDeleteModelVisible = true
+        this.positionTypeDeleteDataId = row.id
       }
     },
-    deletdata(row) {
-      if (this.multipleSelection === '') {
-        this.$message.error('请至少选择一条数据')
-      } else {
-        this.removeData = row
-        this.removeQuestionVisible = true
-      }
+    // 提交删除
+    submitDeletepositionType() {
+      this.$axios.delete('/api/Meta/PositionType/' + this.positionTypeDeleteDataId).then(res => {
+        this.getData()
+        this.$message.success('删除成功')
+        this.positionTypeDeleteModelVisible = false
+      })
     },
-    // 删除试题
-    removeQuestion() {
-      // let _this = this
-      // this.$ajax.delete('/api/services/app/Question/DeleteClozeQuestion?Id=' + this.removeData.id).then(response => {
-      //   if (response.data.success) {
-      //     let index = _this.tableData.indexOf(_this.removeData)
-      //     _this.tableData.splice(index, 1)
-      //     _this.$message.success('删除成功')
-      //     _this.removeQuestionVisible = false
-      //   } else {
-      //     _this.$message.error(response.data.error.message)
-      //   }
-      // })
-    },
-    handleSelectionChange(val) {
-      this.multipleSelection = val
+    // 表单多选数据
+    handleSelectionChangePositionType(val) {
+      this.multipleSelectionPositionType = val
     }
   }
 }
