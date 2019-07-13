479c6125a280d7c1065044eaeb73292bd228ff9f
qingyun <zheng2222021713@hotmail.com>
2019-07-13 16:26:52 +0800

    设备集成商

--- src/views/Assetsetting/systemIntegrate.vue
+++ src/views/Assetsetting/systemIntegrate.vue
@@ -9,12 +9,12 @@
               <el-input v-model="SIFormSearce.text" placeholder="全局查询" size="small" />
               <el-button type="primary" size="small" @click="getData()">查询</el-button>
               <el-button type="success" size="small" @click="adddata()">添加</el-button>
-              <el-button type="warning" size="small" @click="updatedata()">修改</el-button>
-              <el-button type="danger" size="small" @click="deletdata()">删除</el-button>
+              <el-button type="warning" size="small" @click="updateSI()">修改</el-button>
+              <el-button type="danger" size="small" @click="deleteSI()">删除</el-button>
             </div>
           </div>
           <div class="content">
-            <el-table :data="SIData" stripe border style="width: 100%" @selection-change="handleSelectionChange">
+            <el-table :data="SIData" stripe border style="width: 100%" @selection-change="handleSelectionChangeSI">
               <el-table-column type="selection" width="40" />
               <el-table-column prop="id" label="序号" />
               <el-table-column prop="name" label="系统集成商名称" />
@@ -29,15 +29,15 @@
               </el-form>
               <span slot="footer" class="dialog-footer">
                 <el-button type="primary" @click="SIFormVisible=false">关闭</el-button>
-                <el-button type="primary" @click="submitData()">提交更改</el-button>
+                <el-button type="primary" @click="submitSI()">提交更改</el-button>
               </span>
             </el-dialog>
 
-            <el-dialog ref="removeData" title="提示" :close-on-press-escape="false" :close-on-click-modal="false" :visible.sync="removeQuestionVisible" width="220px">
+            <el-dialog ref="removeData" title="提示" :close-on-press-escape="false" :close-on-click-modal="false" :visible.sync="SIDeleteModelVisible" width="220px">
               <span>您确定要删除此条数据？</span>
               <span slot="footer" class="dialog-footer">
-                <el-button @click="removeQuestionVisible = false">取 消</el-button>
-                <el-button type="primary" @click="removeQuestion">确 定</el-button>
+                <el-button @click="SIDeleteModelVisible = false">取 消</el-button>
+                <el-button type="primary" @click="submitDeleteSI">确 定</el-button>
               </span>
             </el-dialog>
           </div>
@@ -75,17 +75,10 @@ export default {
           trigger: 'blur'
         }
       },
-      // ////////////////////
-      removeData: null, // 当前表单所选删除行
-      tableData: [], // 全部数据
-      removeQuestionVisible: false, // 删除弹框隐藏
-      searchMessage: '', // 全局搜索的值
-      multipleSelection: '', // 当前表单所选行val
-      changeActiveVisible: false, // 添加弹出框隐藏
-      title: '添加系统集成商', // 弹框标题
-      formSearch: {// 弹框表单数据
-        type: ''
-      }
+      SIDeleteModelVisible: false,
+      SIDeleteDataId: null,
+      multipleSelectionpSI: []
+
     }
   },
   computed: {},
@@ -109,63 +102,78 @@ export default {
       // 调用获取数据
       this.getData()
     },
+    // 表单关闭重置
+    SIFormClose() {
+      this.$refs.SIForm.resetFields()
+    },
     // 添加
     adddata() {
       this.SIFormVisible = true// 显示弹框
       this.SIFormTitle = '添加设备集成商'
     },
-    // 提交表单
-    submitData() {
-      // 添加弹出框点确认方法
+    updateSI(row) {
+      if (row === undefined) {
+        this.SIFormTitle = '编辑'
+        if (this.multipleSelectionSI.length !== 1) {
+          this.$message.error('请选择一项数据进行操作')
+        } else {
+          this.SIFormVisible = true
+          this.SIForm.id = this.multipleSelectionSI[0].id
+          this.SIForm.name = this.multipleSelectionSI[0].name
+        }
+      } else {
+        this.SIFormVisible = true
+        this.SIForm.id = row.id
+        this.SIForm.name = row.name
+      }
+    },
+    // 表单提交
+    submitSI() {
       this.$refs.SIForm.validate(valid => {
         if (valid) {
-          this.$axios.post('/', this.SIForm).then(res => {
-            this.getSIData()
-            this.SIFormVisible = false
-          })
+          if (this.SIForm.id === undefined) {
+            this.$axios.post('/api/Meta/SI', this.SIForm).then(res => {
+              this.getData()
+              this.$message.success('添加成功')
+              this.SIFormVisible = false
+            })
+          } else {
+            this.$axios.put('/api/Meta/SI/' + this.SIForm.id, this.SIForm).then(res => {
+              this.getData()
+              this.$message.success('修改成功')
+              this.SIFormVisible = false
+            })
+          }
         }
       })
     },
-    // 表单关闭重置
-    SIFormClose() {
-      this.$refs.SIForm.resetFields()
-    },
-    // ////////////////////////////////////////////
-
-    updatedata() {
-      // 修改方法
-      if (this.multipleSelection === '') {
-        this.$message.error('请至少选择一条数据')
+    // 删除
+    deleteSI(row) {
+      if (row === undefined) {
+        if (this.multipleSelectionSI.length !== 1) {
+          this.$message.error('请选择一项数据进行操作')
+        } else {
+          this.SIDeleteModelVisible = true
+          this.SIDeleteDataId = this.multipleSelectionSI[0].id
+        }
       } else {
-        this.title = '编辑系统集成商'
-        this.changeActiveVisible = true// 显示弹框
+        this.SIDeleteModelVisible = true
+        this.SIDeleteDataId = row.id
       }
     },
-    deletdata(row) {
-      if (this.multipleSelection === '') {
-        this.$message.error('请至少选择一条数据')
-      } else {
-        this.removeData = row
-        this.removeQuestionVisible = true
-      }
+    // 提交删除
+    submitDeleteSI() {
+      this.$axios.delete('/api/Meta/SI/' + this.SIDeleteDataId).then(res => {
+        this.getData()
+        this.$message.success('删除成功')
+        this.SIDeleteModelVisible = false
+      })
     },
-    // 删除试题
-    removeQuestion() {
-      // let _this = this
-      // this.$ajax.delete('/api/services/app/Question/DeleteClozeQuestion?Id=' + this.removeData.id).then(response => {
-      //   if (response.data.success) {
-      //     let index = _this.tableData.indexOf(_this.removeData)
-      //     _this.tableData.splice(index, 1)
-      //     _this.$message.success('删除成功')
-      //     _this.removeQuestionVisible = false
-      //   } else {
-      //     _this.$message.error(response.data.error.message)
-      //   }
-      // })
-    },
-    handleSelectionChange(val) {
-      this.multipleSelection = val
+    // 表单多选数据
+    handleSelectionChangeSI(val) {
+      this.multipleSelectionSI = val
     }
+
   }
 }
 </script>
