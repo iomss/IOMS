72d383038076b572a2cb5058e7f320bd0014cd9a
青云 <zheng2222021713@hotmail.com>
2019-07-15 13:32:09 +0800

    position 位置增删改完善

--- src/views/Meta/Position.vue
+++ src/views/Meta/Position.vue
@@ -18,8 +18,12 @@
               <el-table-column type="selection" width="40" />
               <el-table-column prop="id" label="序号" />
               <el-table-column prop="name" label="位置" />
-              <el-table-column prop="" label="上级位置" />
-              <el-table-column prop="" label="位置类型" />
+              <el-table-column prop="parentName" label="上级位置" />
+              <el-table-column prop="" label="位置类型">
+                <template slot-scope="scope">
+                  {{ scope.row.positionType.name }}
+                </template>
+              </el-table-column>
             </el-table>
             <pagination v-show="positionTotalCount>0" :total="positionTotalCount" :page.sync="positionFormSearce.pageNumber" :limit.sync="positionFormSearce.pageSize" @pagination="getpositionPage" />
 
@@ -29,33 +33,9 @@
                   <el-input v-model="positionForm.name" placeholder="位置" size="small" />
                 </el-form-item>
                 <el-form-item label="上级位置">
-                  <TreeSelect v-model="positionForm.parentId" :tree-data="positionTreeData" tree-node-label-prop="name">
-                    <span slot="title" slot-scope="{key, value,node}" style="color: #08c">
-                      {{ node.name }}
-                    </span>
-                  </TreeSelect>
-                  <!-- <a-tree-select show-search style=" width: 300px" :value="value" :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }" placeholder="Please select" allow-clear multiple tree-default-expand-all>
-                      <a-tree-select-node key="0-1" value="parent 1" title="parent 1">
-                        <a-tree-select-node key="0-1-1" value="parent 1-0" title="parent 1-0">
-                          <a-tree-select-node key="random" value="leaf1" title="my leaf" />
-                          <a-tree-select-node key="random1" value="leaf2" title="your leaf" />
-                        </a-tree-select-node>
-                        <a-tree-select-node key="random2" value="parent 1-1" title="parent 1-1">
-                          <a-tree-select-node key="random3" value="sss">
-                            <b slot="title" style="color: #08c">sss</b>
-                          </a-tree-select-node>
-                        </a-tree-select-node>
-                      </a-tree-select-node> -->
-                  <!-- </a-tree-select>
-                  <treeselect v-model="positionForm.parentId" :options="positionTreeData" search-nested>
-                    <div slot="value-label" slot-scope="{ node }">{{ node.raw.name }}</div>
-                    <label slot="option-label" slot-scope="{ node }">
-                      {{ node.raw.name }}
-                    </label>
-                  </treeselect>
-                  </a-tree-select>-->
+                  <treeselect v-model="positionForm.parentId" :normalizer="normalizer" :options="positionTreeData" :load-options="loadOptions" placeholder="请选择上级位置" no-results-text="未找到相关数据" />
                 </el-form-item>
-                <el-form-item label=" 位置类型">
+                <el-form-item label=" 位置类型" prop="positionTypeId">
                   <el-select v-model="positionForm.positionTypeId" size="small" filterable placeholder="请选择">
                     <el-option v-for="item in postiontTypeData" :key="item.id" :label="item.name" :value="item.id" />
                   </el-select>
@@ -81,18 +61,28 @@
   </div>
 </template>
 <script>
-import Vue from 'vue'
-import { TreeSelect } from 'ant-design-vue'
-Vue.use(TreeSelect)
-import 'ant-design-vue/lib/tree-select/style/css'
+import { Treeselect, LOAD_CHILDREN_OPTIONS } from '@riophae/vue-treeselect'
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
 import pagination from '@/components/Pagination/index.vue'
+// 节流
+const simulateAsyncOperation = fn => {
+  setTimeout(fn, 500)
+}
 export default {
   components: {
     pagination,
-    TreeSelect
+    Treeselect
   },
   data() {
     return {
+      // 树结构
+      normalizer(node) {
+        return {
+          id: node.id,
+          label: node.name,
+          children: node.children
+        }
+      },
       positionData: [], // 数据
       positionTreeData: [],
       postiontTypeData: [],
@@ -115,6 +105,11 @@ export default {
           required: true,
           message: '位置名称不可为空',
           trigger: 'blur'
+        },
+        positionTypeId: {
+          required: true,
+          message: '位置类型不可为空',
+          trigger: 'blur'
         }
       },
       positionDeleteModelVisible: false,
@@ -128,6 +123,42 @@ export default {
     this.getData()
   },
   methods: {
+    // treeSelect 加载
+    loadOptions({ action, parentNode, callback }) {
+      if (action === LOAD_CHILDREN_OPTIONS) {
+        switch (parentNode.id) {
+          case 'success': {
+            debugger
+            simulateAsyncOperation(() => {
+              parentNode.children = [
+                {
+                  id: 'child',
+                  label: 'Child option'
+                }
+              ]
+              callback()
+            })
+            break
+          }
+          case 'no-children': {
+            debugger
+            simulateAsyncOperation(() => {
+              parentNode.children = []
+              callback()
+            })
+            break
+          }
+          case 'failure': {
+            debugger
+            simulateAsyncOperation(() => {
+              callback(new Error('Failed to load options: network error.'))
+            })
+            break
+          }
+          default: /* empty */
+        }
+      }
+    },
     // 获取数据
     getData() {
       this.$axios.get('/api/Meta/Position', { params: this.positionFormSearce }).then(res => {
@@ -168,18 +199,20 @@ export default {
         if (this.multipleSelectionPosition.length !== 1) {
           this.$message.error('请选择一项数据进行操作')
         } else {
+          debugger
           this.positionFormVisible = true
           this.positionForm.id = this.multipleSelectionPosition[0].id
           this.positionForm.name = this.multipleSelectionPosition[0].name
-          this.positionForm.parentId = this.multipleSelectionpPosition[0].parentId
-          this.positionForm.positionTypeId = this.multipleSelectionpPosition[0].positionTypeId
+          this.positionForm.parentId = this.multipleSelectionPosition[0].parentId
+          this.positionForm.positionTypeId = this.multipleSelectionPosition[0].positionType.id
         }
       } else {
+        debugger
         this.PositionFormVisible = true
         this.PositionForm.id = row.id
         this.PositionForm.name = row.name
         this.positionForm.parentId = row.parentId
-        this.positionForm.positionTypeId = row.positionTypeId
+        this.positionForm.positionTypeId = row.positionType.id
       }
     },
     // 表单提交
