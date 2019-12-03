ca4d2ba24dae4aed2ab7fbe0661436ec67408b45
qianqianwanlong <734908350@qq.com>
2019-12-03 18:06:09 +0800

    修改 权限

--- src/views/emergencyRepair/components/approvalView.vue
+++ src/views/emergencyRepair/components/approvalView.vue
@@ -282,7 +282,7 @@ export default {
       if (this.queryRoles.EmergencyRequisitionSubCenterReview && this.viewDesc.emergencyState === 'PendingSubCenter') {
         this.emergencyRequisition(this.subCenter)
         // 路网中心审核
-      } else if (this.queryRoles.EmergencyRequisitionNetCenterEngineerReview && this.viewDesc.emergencyState === 'PendingNetCenter' && (this.viewDesc.reviewerId === null || this.viewDesc.reviewerId === currentUserId)) {
+      } else if (this.queryRoles.EmergencyRequisitionNetCenterEngineerReview && this.viewDesc.emergencyState === 'PendingNetCenter' && (this.viewDesc.reviewerId === null || this.viewDesc.reviewerId.toString() === currentUserId)) {
         this.emergencyRequisition(this.pattern)
         // 分管领导意见
       } else if (this.queryRoles.EmergencyRequisitionLeaderReview && this.viewDesc.emergencyState === 'PendingLeader') {
